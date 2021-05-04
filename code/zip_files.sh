bf02976eafc8d43abbc18e97fccca87a2b484f29
jckuri <jckuri@gmail.com>
2021-05-03 23:56:29 -0500

    second commit

--- code/zip_files.sh
+++ code/zip_files.sh
@@ -1,8 +1,9 @@
 rm RealSDC.zip
-wp_updater="CarND-Capstone/ros/src/waypoint_updater/waypoint_updater.py"
-dbw_node="CarND-Capstone/ros/src/twist_controller/dbw_node.py"
-twist_controller="CarND-Capstone/ros/src/twist_controller/twist_controller.py"
-tl_detector="CarND-Capstone/ros/src/tl_detector/tl_detector.py"
-tl_classifier="CarND-Capstone/ros/src/tl_detector/light_classification/tl_classifier.py"
-model="CarND-Capstone/ros/src/tl_detector/light_classification/frozen_inference_graph.pb"
-zip -j RealSDC.zip $wp_updater $dbw_node $twist_controller $tl_detector $tl_classifier $model zip_files.sh
+SDC_DIR="/home/workspace/CarND-Capstone/ros/src"
+wp_updater="$SDC_DIR/waypoint_updater/waypoint_updater.py"
+dbw_node="$SDC_DIR/twist_controller/dbw_node.py"
+twist_controller="$SDC_DIR/twist_controller/twist_controller.py"
+tl_detector="$SDC_DIR/tl_detector/tl_detector.py"
+tl_classifier="$SDC_DIR/tl_detector/light_classification/tl_classifier.py"
+model="$SDC_DIR/tl_detector/light_classification/frozen_inference_graph.pb"
+zip -j RealSDC.zip $wp_updater $dbw_node $twist_controller $tl_detector $tl_classifier $model
